dbbcf6354bb809b578947611d65edadfd6b8fd12
TomasSorgetti <tomassorgetti456@gmail.com>
2024-10-06 23:19:52 -0300

    styles(animations): Se modificaron las animaciones de los buttons y de las cards de categorias

--- src/components/ui/buttons/cardButton/CardButton.jsx
+++ src/components/ui/buttons/cardButton/CardButton.jsx
@@ -4,6 +4,7 @@ import arrowRight from "../../../../assets/icons/arrow_right.svg";
 export default function CardButton({ children, path }) {
   return (
     <Link className={styles.button} to={path}>
+      <div className={styles.bar}></div>
       {children}
       <img src={arrowRight} alt="arrow icon" />
     </Link>
